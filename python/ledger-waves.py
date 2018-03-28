8cb4454e3ebb1032e4b4118066cb397b0b581e60
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-03-28 19:19:03 +0300

    wip on getting address by bip32 path

--- python/ledger-waves.py
+++ python/ledger-waves.py
@@ -76,7 +76,7 @@ publicKey = None
 privateKey = None
 
 
-def getKeysFromDongle():
+def getKeysFromDongle(path):
     global publicKey
     global dongle
     if (publicKey):
@@ -86,8 +86,8 @@ def getKeysFromDongle():
             try:
                 if (dongle == None):
                     dongle = getDongle(True)
-                data = dongle.exchange(bytes("8004000000".decode('hex')))
-                return [data[0:32], data[32:64], data[64:128]]
+                data = dongle.exchange(bytes("8004800014".decode('hex')) + path_to_bytes(path))
+                return [data[0:32], data[32:64]]
             except Exception as e:
                 print e
                 answer = raw_input(
@@ -97,6 +97,44 @@ def getKeysFromDongle():
                 sys.exc_clear()
 
 CHUNK_SIZE = 128
+PRIME_DERIVATION_FLAG = 0x80000000
+
+def path_to_bytes(path):
+    return ''.join([struct.pack(">I", n) for n in path])
+
+def convert_prime(n):
+    # Convert minus signs to uint32 with flag
+    return [int(abs(x) | PRIME_DERIVATION_FLAG) if x < 0 else x for x in n]
+
+def expand_path(n):
+    # Convert string of bip32 path to list of uint32 integers with prime flags
+    # 0/-1/1' -> [0, 0x80000001, 0x80000001]
+    if not n:
+        return []
+
+    n = n.split('/')
+
+    # m/a/b/c => a/b/c
+    if n[0] == 'm':
+        n = n[1:]
+
+    path = []
+    for x in n:
+        prime = False
+        if x.endswith("'"):
+            x = x.replace('\'', '')
+            prime = True
+        if x.startswith('-'):
+            prime = True
+
+        x = abs(int(x))
+
+        if prime:
+            x |= PRIME_DERIVATION_FLAG
+
+        path.append(x)
+
+    return path
 
 while (True):
     print("")
@@ -107,14 +145,12 @@ while (True):
     select = raw_input(colors.fg.cyan + "Please select> " + colors.reset)
 
     if (select == "1"):
-        keys = getKeysFromDongle()
+        keys = getKeysFromDongle(expand_path("m/44'/5741564'/0'/0'/1'"))
         publicKey = keys[0]
-        privateKey = keys[1]
-        signature = keys[2]
+        address = keys[1]
 
         print colors.fg.blue + "publicKey: " + colors.reset + str(publicKey).encode('hex')
-        print colors.fg.blue + "privateKey: " + colors.reset + str(privateKey).encode('hex')
-        print colors.fg.blue + "signature: " + colors.reset + str(signature).encode('hex')
+        print colors.fg.blue + "address: " + colors.reset + str(address).encode('hex')
     elif (select == "2"):
         binary_data = bytes("a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad03a548b271aaec21b6297e381f22e69327c4cb135087c93683e60a1cf820d4c52a0619d4ef7330f9cedd69eda092839d21c8cee1891025e760c8aaab57b4a6ad0311111111".decode('hex'))
         signature = None

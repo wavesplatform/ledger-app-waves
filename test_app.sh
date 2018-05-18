5cbae88d3f9de800c2e44b862073ad4153e66e44
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-05-18 16:39:27 +0300

    ram used info added to error message

--- test_app.sh
+++ test_app.sh
@@ -2,6 +2,6 @@
 
 RAM_USED=$(grep '0x300 00' debug/app.map | awk '{print $2}');
 if [[ "$RAM_USED" > "0x20002100" ]]; then
-    echo "Ram limit reached in a compiled app. Fix it!";
+    echo "Ram limit reached in a compiled app ($RAM_USED) !. Fix it!";
     exit -1;
 fi;

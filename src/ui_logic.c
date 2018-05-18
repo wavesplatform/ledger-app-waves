ccd6961f0b7a0bd7b52559169aca84251fa772aa
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-05-18 13:44:28 +0300

    use constants
    and the comments was removed

--- src/ui_logic.c
+++ src/ui_logic.c
@@ -21,8 +21,12 @@ unsigned int io_seproxyhal_touch_address_ok(const bagl_element_t *e) {
 }
 
 unsigned int io_seproxyhal_touch_address_cancel(const bagl_element_t *e) {
-    G_io_apdu_buffer[0] = 0x69;
-    G_io_apdu_buffer[1] = 0x85;
+    uint32_t tx = 0;
+
+    unsigned short sw = SW_CONDITIONS_NOT_SATISFIED
+
+    G_io_apdu_buffer[tx++] = sw >> 8;
+    G_io_apdu_buffer[tx++] = sw;
 
     // Send back the response, do not restart the event loop
     io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
@@ -54,21 +58,13 @@ unsigned int io_seproxyhal_touch_verify_transfer_approve(const bagl_element_t *e
 }
 
 unsigned int io_seproxyhal_touch_verify_transfer_deny(const bagl_element_t *e) {
-    G_io_apdu_buffer[0] = 0x69;
-    G_io_apdu_buffer[1] = 0x85;
-    // Send back the response, do not restart the event loop
-    // todo u2f?
-//    #if HAVE_U2F
-//        if (fido_activated) {
-//            u2f_proxy_response((u2f_service_t *)&u2fService, 2);
-//        } else {
-//    #endif
-//            // Send back the response, do not restart the event loop
+    uint32_t tx = 0;
+    unsigned short sw = SW_CONDITIONS_NOT_SATISFIED
+
+    G_io_apdu_buffer[tx++] = sw >> 8;
+    G_io_apdu_buffer[tx++] = sw;
+
     io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
-//            #if HAVE_U2F
-//        } // close fido_activated if statement
-//    #endif
-    // Display back the original UX
 
     init_context();
 

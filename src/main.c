efc556673b87f4a6e608dfcfb8fd533859a8f490
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-12-07 16:54:54 +0100

    Update os_global_pin_is_validated check

--- src/main.c
+++ src/main.c
@@ -217,7 +217,7 @@ void handle_apdu(volatile unsigned int *flags, volatile unsigned int *tx,
   BEGIN_TRY {
     TRY {
 
-      if (os_global_pin_is_validated() == 0) {
+      if (os_global_pin_is_validated() != BOLOS_TRUE) {
         THROW(SW_DEVICE_IS_LOCKED);
       }
 
